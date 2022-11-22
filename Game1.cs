d28746f544b6e3b3017042ec2c283dc43a152117
Doom-Art <ouginak121@gmail.com>
2022-11-22 09:13:58 -0500

    added countdown and explosion pic/exits after sound

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading;
 
 namespace Monogame_4_Time___Sound
 {
@@ -12,9 +13,14 @@ namespace Monogame_4_Time___Sound
         private Texture2D bombTexture;
         SpriteFont _font;
         float seconds;
+        float countdown;
         float startTime;
         MouseState mouseState;
         SoundEffect explode;
+        bool explosion;
+        Texture2D boom;
+        float timer;
+        bool timerSet;
 
         public Game1()
         {
@@ -29,6 +35,8 @@ namespace Monogame_4_Time___Sound
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.ApplyChanges();
             this.Window.Title = "Bomb";
+            explosion = false;
+            bool timerSet = false;
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -40,6 +48,7 @@ namespace Monogame_4_Time___Sound
             bombTexture = Content.Load<Texture2D>("bomb");
             _font = Content.Load<SpriteFont>("Time");
             explode = Content.Load<SoundEffect>("explosion");
+            boom = Content.Load<Texture2D>("boom");
             // TODO: use this.Content to load your game content here
         }
 
@@ -48,14 +57,28 @@ namespace Monogame_4_Time___Sound
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             seconds = (float)gameTime.TotalGameTime.TotalSeconds - startTime;
-            if (mouseState.LeftButton == ButtonState.Pressed)
+            countdown = 15 - seconds;
+            if (mouseState.LeftButton == ButtonState.Pressed){
                 startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+                explosion = false;
+            }
             if (mouseState.RightButton == ButtonState.Pressed)
-                startTime = (float)gameTime.TotalGameTime.TotalSeconds - 9;
-            if (seconds >= 10){
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds - 14;
+            if (seconds >= 15){
                 explode.Play();
+                explosion = true;
                 startTime = (float)gameTime.TotalGameTime.TotalSeconds;
             }
+            if (explosion && !timerSet)
+            {
+                timer = (float)gameTime.TotalGameTime.TotalSeconds + (float)explode.Duration.TotalSeconds;
+                timerSet = true;
+            }
+            if (timer <= (float)gameTime.TotalGameTime.TotalSeconds && explosion)
+                Exit();
+            if (explosion)
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+
             mouseState = Mouse.GetState();
             // TODO: Add your update logic here.
 
@@ -66,8 +89,12 @@ namespace Monogame_4_Time___Sound
         {
             GraphicsDevice.Clear(Color.Cyan);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(bombTexture, new Rectangle(50, 50, 700, 400), Color.White);
-            _spriteBatch.DrawString(_font, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
+            if (!explosion){
+                _spriteBatch.Draw(bombTexture, new Rectangle(50, 50, 700, 400), Color.White);
+                _spriteBatch.DrawString(_font, countdown.ToString("00.0"), new Vector2(270, 200), Color.Black);
+            }
+            else
+                _spriteBatch.Draw(boom, new Rectangle(-90,-65, 1000,600), Color.White);
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
